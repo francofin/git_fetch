3b7a3c16d3194d5f76c5879d5f8ad22c20880180
fintank <finapptank@gmail.com>
2020-10-10 14:19:39 -0400

    added single page js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,17 +1,95 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var displayRepos = function(repos, searchTerm) {
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+  // check if api returned any repos
+  
+  // loop over repos
+  for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+    var repo = repoName;
+
+    // create a container for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo="+repo);
+    // repo refers to the variable created to store the data
+    //use the location web api to link the pages, use document.location to locate the query string
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
+        // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    // append to container
+    repoEl.appendChild(statusEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+  console.log(repos);
+  console.log(searchTerm);
+};
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+      getUserRepos(username);
+      nameInputEl.value = "";
+    } else {
+      alert("Please enter a GitHub username");
+    }
+  console.log(event);
+};
+
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+      if (response.ok) {
       response.json().then(function(data) {
-        console.log(data);
+        displayRepos(data, user);
       });
+    } else {
+      alert("Error:" + response.statusText);
+    }
+    })
+    .catch(function(error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
     });
   };
 
-  getUserRepos("facebook");
-  getUserRepos("microsoft");
+  userFormEl.addEventListener("submit", formSubmitHandler);
 
 
 
@@ -26,7 +104,8 @@ var getUserRepos = function(user) {
 
 
 
-
+//query parameters allow us to connect information
+// we use ? to append query parameters
 
   // using ajax to load same getUserRepo request $.ajax("https://api.github.com/users/octocat/repos").done(function(data) {console.log(data);});
 
