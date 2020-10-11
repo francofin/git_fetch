fe991af0b287e26351c3313801a39ea17c3cdbd2
fintank <finapptank@gmail.com>
2020-10-10 22:19:59 -0400

    added language search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,8 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-form");
+
 
 var displayRepos = function(repos, searchTerm) {
   if (repos.length === 0) {
@@ -54,6 +56,21 @@ var displayRepos = function(repos, searchTerm) {
   console.log(searchTerm);
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+        console.log(data)
+      });
+     } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
 var formSubmitHandler = function(event) {
   event.preventDefault();
   // get value from input element
@@ -68,6 +85,16 @@ var formSubmitHandler = function(event) {
   console.log(event);
 };
 
+var selectClickHandler = function(event) {
+  event.preventDefault();
+  var language = document.querySelector("select[name='languages']").value;
+  if (language) {
+    getFeaturedRepos(language);
+    repoContainerEl.textContent = "";
+    console.log(language)
+  };
+  
+}
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -90,7 +117,7 @@ var getUserRepos = function(user) {
   };
 
   userFormEl.addEventListener("submit", formSubmitHandler);
-
+  languageButtonsEl.addEventListener("submit", selectClickHandler);
 
 
 
